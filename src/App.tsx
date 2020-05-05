a89dbd74eb243e655351ee5979e08cd6fe7c15d7
Naoki Mita <naoki.mita18@gmail.com>
2020-05-05 15:19:48 -0700

    Can shuffle words

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,26 @@ import "./App.css";
 import { Button } from "@material-ui/core/";
 import { Language, PhoneIphone, Phone, MusicNote } from "@material-ui/icons/";
 
-function App() {
+var seenWords: Set<number> = new Set();
+const words: string[] = ["Telphone", "Music", "Internet", "AI", "Book", "Fashion", "Social Network", "Delivery", "Taxi", "Software Enginner"];
+
+function getWord(): string {
+  var i: number = Math.floor(Math.random() * words.length);
+  while (seenWords.has(i)) {
+    i = Math.floor(Math.random() * words.length);
+  }
+  seenWords.add(i);
+  return words[i];
+}
+
+const App: React.FC = () => {
+  const [threeWords, setThreeWords] = React.useState<string[]>([getWord(), getWord(), getWord()]);
+
+  function resetWords(e: any) {
+    seenWords = new Set();
+    setThreeWords([getWord(), getWord(), getWord()]);
+  }
+
   return (
     <div className='App'>
       <header>
@@ -20,19 +39,19 @@ function App() {
           <PhoneIphone /> iPhone
         </p>
         <ul className='slot-words'>
-          <li>Telephone</li>
+          <li>{threeWords[0]}</li>
           <li className='right-border middle-li'></li>
-          <li>Music</li>
+          <li>{threeWords[1]}</li>
           <li className='right-border middle-li'></li>
-          <li>Internet</li>
+          <li>{threeWords[2]}</li>
         </ul>
-        <Button variant='contained' color='primary'>
+        <Button variant='contained' color='primary' onClick={resetWords}>
           Shuffle
         </Button>
       </article>
-      <footer>2020 © ideabank.com All Rights Reserved.</footer>
+      <footer>2020 © **** All Rights Reserved.</footer>
     </div>
   );
-}
+};
 
 export default App;
