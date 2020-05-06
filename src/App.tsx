76361e72606b7ffd4ef3ceec98bee1e6b77e5189
Naoki Mita <naoki.mita18@gmail.com>
2020-05-05 21:09:02 -0700

    Shuffle words smoothly for design

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./App.css";
 import { Button } from "@material-ui/core/";
 import { Language, PhoneIphone, Phone, MusicNote } from "@material-ui/icons/";
@@ -17,10 +17,46 @@ function getWord(): string {
 
 const App: React.FC = () => {
   const [threeWords, setThreeWords] = React.useState<string[]>([getWord(), getWord(), getWord()]);
+  const wordRefs = React.useRef<any>([React.createRef(), React.createRef(), React.createRef()]);
 
   function resetWords(e: any) {
-    seenWords = new Set();
-    setThreeWords([getWord(), getWord(), getWord()]);
+    Promise.all(
+      wordRefs.current.map(async (ref: any) => {
+        return await changeWordStyle(ref);
+      })
+    ).then((results) => {
+      results.forEach((result) => {
+        if (result !== "success") {
+          console.error("shuffle words error...");
+          return;
+        }
+      });
+      seenWords = new Set();
+      setThreeWords([getWord(), getWord(), getWord()]);
+    });
+  }
+
+  async function changeWordStyle(ref: any) {
+    ref.current.style.transform = "translateY(-100%)";
+    ref.current.style.opacity = 0;
+    return await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        ref.current.style.transitionDuration = "0s";
+        ref.current.style.transform = "translateY(100%)";
+        ref.current.style.opacity = 0;
+
+        setTimeout(() => {
+          ref.current.style.transitionDuration = "0.3s";
+          ref.current.style.transform = "translateY(0)";
+          ref.current.style.opacity = 1;
+          try {
+            resolve("success");
+          } catch (err) {
+            reject(err);
+          }
+        }, 20);
+      }, 250);
+    });
   }
 
   return (
@@ -39,11 +75,19 @@ const App: React.FC = () => {
           <PhoneIphone /> iPhone
         </p>
         <ul className='slot-words'>
-          <li>{threeWords[0]}</li>
-          <li className='right-border middle-li'></li>
-          <li>{threeWords[1]}</li>
-          <li className='right-border middle-li'></li>
-          <li>{threeWords[2]}</li>
+          {threeWords.length > 0 &&
+            threeWords.map((word: string, i: number) => (
+              <Fragment key={i}>
+                <li className='slot-word-li'>
+                  <p ref={wordRefs.current[i]}>{word}</p>
+                </li>
+                {i < threeWords.length - 1 && (
+                  <li className='middle-li'>
+                    <p>×</p>
+                  </li>
+                )}
+              </Fragment>
+            ))}
         </ul>
         <Button variant='contained' color='primary' onClick={resetWords}>
           Shuffle
