2489ac7cfeaacb990bf232fc48985920b2d32225
Naoki Mita <naoki.mita18@gmail.com>
2020-06-05 15:28:31 -0700

    Rename ideabank to ideapool

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDfNx1pCONd0mMx-eGV1zBpyZRBdsm3eqk",
+  authDomain: "ideabank-1887f.firebaseapp.com",
+  databaseURL: "https://ideabank-1887f.firebaseio.com",
+  projectId: "ideabank-1887f",
+  storageBucket: "ideabank-1887f.appspot.com",
+  messagingSenderId: "452663767600",
+  appId: "1:452663767600:web:8207df5e8decc2a2ae33f5",
+  measurementId: "G-7XJB6XMT3Q",
+};
+
+export default firebaseConfig;
