360a792e82ab9aaaac31783bd4ea19e61c760ef0
Naoki Mita <naoki.mita18@gmail.com>
2020-07-15 21:55:04 -0700

    Able to move my list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import useAuth from "./component/context/useAuth";
-import { auth, database, FirebaseContext } from "./firebase";
-import TopPage from "./component/top/Top";
-import LoginPage from "./component/login/LoginPage";
-import Header from "./component/common/Header";
-import Footer from "./component/common/Footer";
-import SignUpPage from "./component/signup/SignUpPage";
-import PrivacyPage from "./component/static/PrivacyPage";
-import TermPage from "./component/static/TermPage";
+import TopPage from "./components/pages/top/Top";
+import LoginPage from "./components/pages/login/LoginPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SignUpPage from "./components/pages/signup/SignUpPage";
+import PrivacyPage from "./components/pages/static/PrivacyPage";
+import TermPage from "./components/pages/static/TermPage";
+import AddIdea from "./components/modal/AddIdea";
+import MyIdeaList from "./components/pages/mylist/MyIdeaList";
+import IdeaContextProvider from "./contexts/IdeaContext";
+import FirebaseContextProvider from "./contexts/FirebaseContext";
 
 const App: React.FC = () => {
-  const user = useAuth();
   return (
-    <FirebaseContext.Provider value={{ user, auth, database }}>
-      <Router>
-        <div className='App'>
-          <Header />
-          <Switch>
-            <Route path='/'>
-              <Switch>
-                <Route exact path='/' component={TopPage} />
-                <Route exact path='/login' component={LoginPage} />
-                <Route exact path='/signup' component={SignUpPage} />
-                <Route exact path='/privacy' component={PrivacyPage} />
-                <Route exact path='/term' component={TermPage} />
-              </Switch>
-            </Route>
-          </Switch>
-        </div>
-        <Footer />
-      </Router>
-    </FirebaseContext.Provider>
+    <FirebaseContextProvider>
+      <IdeaContextProvider>
+        <Router>
+          <div className='App'>
+            <Header />
+            <Switch>
+              <Route path='/'>
+                <Switch>
+                  <Route exact path='/' component={TopPage} />
+                  <Route exact path='/add' component={AddIdea} />
+                  <Route exact path='/my_ideas' component={MyIdeaList} />
+                  <Route exact path='/login' component={LoginPage} />
+                  <Route exact path='/signup' component={SignUpPage} />
+                  <Route exact path='/privacy' component={PrivacyPage} />
+                  <Route exact path='/term' component={TermPage} />
+                </Switch>
+              </Route>
+            </Switch>
+          </div>
+          <Footer />
+        </Router>
+      </IdeaContextProvider>
+    </FirebaseContextProvider>
   );
 };
 
