c744aec11f8295a456423f468b1e5144637d01d0
Naoki Mita <naoki.mita18@gmail.com>
2020-07-23 23:17:54 -0700

    Delete typing animation and any

--- src/components/pages/top/Top.tsx
+++ src/components/pages/top/Top.tsx
@@ -5,85 +5,32 @@ import { words } from "../../../data/words";
 import { Shuffle } from "@material-ui/icons/";
 
 var seenWords: Set<string> = new Set();
-const initialWords = [getWord(), getWord(), getWord()];
 
-function getWord(): string {
+const getWord = (): string => {
   var i: number = Math.floor(Math.random() * words.length);
   while (seenWords.has(words[i])) {
     i = Math.floor(Math.random() * words.length);
   }
   seenWords.add(words[i]);
   return words[i];
-}
+};
+
+const initialWords = [getWord(), getWord(), getWord()];
 
 const TopPage: React.FC = () => {
   const [shuffleWords, setShuffleWords] = React.useState<string[]>(initialWords);
-  const wordRefs = React.useRef<any>([React.useRef(), React.useRef(), React.useRef()]);
-  const [fixedSwitch, setFixedSwitch] = React.useState([false, false, false]);
-
-  const typingIntervalRef = React.useRef<any>();
-  const cursorIntervalRef = React.useRef<any>();
-  const [typingRefs, settypingRefs] = React.useState<any[]>([React.useRef(), React.useRef(), React.useRef()]);
-  const [typingTextArr, settypingTextArr] = React.useState<string[]>(["", "", ""]);
-  const [index, setIndex] = React.useState(0);
-  const [count, setCount] = React.useState(0);
-  const [isCursor, setIsCursor] = React.useState(true);
-  const [isShuffleBtn, setIsShuffleBtn] = React.useState(true);
-
-  const typingTextData = [
-    "Let's think creatively!",
-    "Try to come up with a new idea by combining the following three words.",
-    "ex.) 📞Telephone　✖️ 🎵Music　✖️ 🌍Internet　= 📱iPhone",
-  ];
-
-  React.useEffect(() => {
-    if (isCursor) {
-      cursorIntervalRef.current = setInterval(() => {
-        let newRefs = typingRefs;
-        let curColor = newRefs[count].current.style.borderColor;
-        newRefs[count].current.style.borderColor = curColor === "black" ? "white" : "black";
-        settypingRefs(newRefs);
-      }, 200);
-    }
-    return () => {
-      clearInterval(cursorIntervalRef.current);
-    };
-  }, [count, isCursor, typingRefs]);
-
-  React.useEffect(() => {
-    typingIntervalRef.current = settypingTextArr(typingTextArr);
-  }, [typingTextArr]);
-
-  React.useEffect(() => {
-    if (count < typingTextData.length) {
-      if (index < typingTextData[count].length) {
-        typingIntervalRef.current = setInterval(() => {
-          let newTextArr: string[] = typingTextArr;
-          newTextArr[count] += typingTextData[count][index];
-          settypingTextArr(newTextArr);
-          setIndex(index + 1);
-        }, 40);
-      } else {
-        setIndex(0);
-        typingRefs[count].current.style.borderColor = "white";
-        setCount(count + 1);
-      }
-    } else {
-      setIsCursor(false);
-    }
-    return () => {
-      clearInterval(typingIntervalRef.current);
-    };
-  }, [count, index, typingRefs, typingTextArr, typingTextData]);
+  const wordRefs = [React.useRef<HTMLInputElement>(null), React.useRef<HTMLInputElement>(null), React.useRef<HTMLInputElement>(null)];
+  const [fixedSwitch, setFixedSwitch] = React.useState<boolean[]>([false, false, false]);
+  const [isShuffleBtn, setIsShuffleBtn] = React.useState<boolean>(true);
 
   React.useEffect(() => {
     let swtchCount: number = fixedSwitch.filter((sc: boolean) => sc).length;
     setIsShuffleBtn(swtchCount !== fixedSwitch.length);
   }, [fixedSwitch, isShuffleBtn]);
 
-  function resetWords(e: any) {
+  const resetWords = () => {
     Promise.all(
-      wordRefs.current.map(async (ref: any, i: number) => {
+      wordRefs.map(async (ref: any, i: number) => {
         if (fixedSwitch[i]) {
           return "success";
         }
@@ -102,17 +49,17 @@ const TopPage: React.FC = () => {
         }
         seenWords.delete(shuffleWords[i]);
       }
-      let newThreeWords = shuffleWords.map((w: any, i: number) => {
+      let newThreeWords = shuffleWords.map((w, i) => {
         return fixedSwitch[i] ? w : getWord();
       });
       setShuffleWords([...newThreeWords]);
     });
-  }
+  };
 
-  async function changeWordStyle(ref: any) {
+  const changeWordStyle = (ref: any) => {
     ref.current.style.transform = "translateY(-100%)";
     ref.current.style.opacity = 0;
-    return await new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
       setTimeout(() => {
         ref.current.style.transitionDuration = "0s";
         ref.current.style.transform = "translateY(100%)";
@@ -129,11 +76,11 @@ const TopPage: React.FC = () => {
         }, 20);
       }, 250);
     });
-  }
+  };
 
   const handleSwitchChange = (num: number) => {
     setFixedSwitch(
-      fixedSwitch.map((sc: any, i: number) => {
+      fixedSwitch.map((sc, i) => {
         return i === num ? !sc : sc;
       })
     );
@@ -141,7 +88,7 @@ const TopPage: React.FC = () => {
 
   const handleTextChange = (num: number, event: React.ChangeEvent<HTMLInputElement>) => {
     setShuffleWords(
-      shuffleWords.map((w: any, i: number) => {
+      shuffleWords.map((w, i) => {
         return i === num ? event.target.value : w;
       })
     );
@@ -149,25 +96,35 @@ const TopPage: React.FC = () => {
 
   return (
     <article>
-      {typingTextArr.map((t: string, i: number) => (
-        <div key={i}>
-          {i === 0 ? (
-            <h2 className='typing-text' ref={typingRefs[i]}>
-              {t}
-            </h2>
-          ) : (
-            <p ref={typingRefs[i]} className='typing-text description'>
-              {t}
-            </p>
-          )}
-        </div>
-      ))}
+      <div>
+        <h2 className='typing-text'>Let's generate a new business idea</h2>
+        <p className='typing-text description'>associates with a new idea by combining the following those words.</p>
+        <p className='typing-text description'>
+          ex.){" "}
+          <span role='img' aria-label='phone'>
+            📞
+          </span>
+          Telephone ✖️
+          <span role='img' aria-label='music'>
+            🎵
+          </span>
+          Music ✖️
+          <span role='img' aria-label='earth'>
+            🌍
+          </span>
+          Internet ={" "}
+          <span role='img' aria-label='smartphone'>
+            📱
+          </span>
+          iPhone
+        </p>
+      </div>
       <ul className='three-elements-container slot-words'>
         {shuffleWords.length > 0 &&
           shuffleWords.map((word: string, i: number) => (
             <Fragment key={i}>
               <li className='slot-word-li'>
-                <input ref={wordRefs.current[i]} value={word} onChange={(e) => handleTextChange(i, e)} />
+                <input ref={wordRefs[i]} value={word} onChange={(e) => handleTextChange(i, e)} />
               </li>
               {i < shuffleWords.length - 1 && (
                 <li className='middle-li'>
