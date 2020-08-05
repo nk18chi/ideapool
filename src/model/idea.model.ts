fe5da49fbcb44ab3dad18c3a79b0ff46b58e6f1d
Naoki Mita <naoki.mita18@gmail.com>
2020-08-04 21:33:12 -0700

    Get createdAt and updatedAt data of ideas

--- src/model/idea.model.ts
+++ src/model/idea.model.ts
@@ -1,4 +1,4 @@
-import * as firebase from "firebase/app";
+import * as firebase from "firebase";
 
 export type TIdeaContext = {
   ideas: TIdeaList[];
@@ -9,10 +9,15 @@ export type TIdeaList = {
   id: string;
   title: string;
   description: string;
+  createdAt: firebase.firestore.Timestamp;
+  updatedAt: firebase.firestore.Timestamp;
 };
 
 export type TIdeaDetail = {
+  id: string;
   title: string;
   description: string;
+  createdAt: firebase.firestore.Timestamp;
+  updatedAt: firebase.firestore.Timestamp;
   user: string;
 };
