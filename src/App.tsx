271b12dfa0ae44f3c57553a35d951f2272e95526
Naoki Mita <naoki.mita18@gmail.com>
2020-08-04 21:35:24 -0700

    Change path for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,47 @@
-import React from "react";
+import React, { FC } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import TopPage from "./components/pages/top/Top";
-import LoginPage from "./components/pages/login/LoginPage";
+import { TopPage } from "./components/pages/TopPage";
+import { SignUpPage } from "./components/pages/SignUpPage";
+import { LoginPage } from "./components/pages/LoginPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SignUpPage from "./components/pages/signup/SignUpPage";
 import PrivacyPage from "./components/pages/static/PrivacyPage";
 import TermPage from "./components/pages/static/TermPage";
 import AddIdea from "./components/modal/AddIdea";
-import MyIdeaList from "./components/pages/mylist/MyIdeaList";
+import MyIdeaList from "./components/pages/MyIdeaList";
 import IdeaContextProvider from "./contexts/IdeaContext";
 import FirebaseContextProvider from "./contexts/FirebaseContext";
+import { GlobalStyles } from "./components/style/GlobalStyles";
+import { StylesProvider } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
     <FirebaseContextProvider>
       <IdeaContextProvider>
         <Router>
-          <div className='App'>
-            <Header />
-            <Switch>
-              <Route path='/'>
+          <StylesProvider injectFirst>
+            <GlobalStyles />
+            <div className='App'>
+              <Header />
+              <Container maxWidth='md'>
                 <Switch>
-                  <Route exact path='/' component={TopPage} />
-                  <Route exact path='/add' component={AddIdea} />
-                  <Route exact path='/my_ideas' component={MyIdeaList} />
-                  <Route exact path='/login' component={LoginPage} />
-                  <Route exact path='/signup' component={SignUpPage} />
-                  <Route exact path='/privacy' component={PrivacyPage} />
-                  <Route exact path='/term' component={TermPage} />
+                  <Route path='/'>
+                    <Switch>
+                      <Route exact path='/' component={TopPage} />
+                      <Route exact path='/add' component={AddIdea} />
+                      <Route exact path='/my_ideas' component={MyIdeaList} />
+                      <Route exact path='/login' component={LoginPage} />
+                      <Route exact path='/signup' component={SignUpPage} />
+                      <Route exact path='/privacy' component={PrivacyPage} />
+                      <Route exact path='/term' component={TermPage} />
+                    </Switch>
+                  </Route>
                 </Switch>
-              </Route>
-            </Switch>
-          </div>
-          <Footer />
+              </Container>
+              <Footer />
+            </div>
+          </StylesProvider>
         </Router>
       </IdeaContextProvider>
     </FirebaseContextProvider>
