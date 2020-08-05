bfb2959046c071526cacb5224c3bfdf3aee1ad63
Naoki Mita <naoki.mita18@gmail.com>
2020-08-04 21:31:49 -0700

    Set default style in emotion

---
+++ src/components/style/Common.style.tsx
@@ -0,0 +1,36 @@
+import { css } from "@emotion/core";
+
+/** @jsx jsx */
+
+export const StyleMainTitle = css`
+  margin: 40px 0 16px;
+  font-size: 32px;
+  font-weight: 800;
+  text-align: center;
+`;
+
+export const StyleInput = css`
+  margin: 8px 0;
+`;
+
+export const StyleMargin4 = css`
+  margin: 4px;
+`;
+
+export const StylePointer = css`
+  cursor: pointer;
+`;
+
+export const StyleSubmitButton = css`
+  margin-top: 20px;
+`;
+
+export const StyleCicularWrapper = css`
+  position: relative;
+`;
+export const StyleCicular = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-left: -12px;
+`;
