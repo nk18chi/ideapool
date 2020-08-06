3187b68c2eb86e9f27fb6558d4d07bcdde638fe4
Naoki Mita <naoki.mita18@gmail.com>
2020-08-06 16:47:00 -0700

    Style Document compomenent with emotion

--- src/components/pages/static/Document.tsx
+++ src/components/pages/static/Document.tsx
@@ -1,6 +1,31 @@
 import React from "react";
-import "./Document.scss";
 import ReactMarkdown from "react-markdown";
+import { jsx, css } from "@emotion/core";
+/** @jsx jsx */
+
+const StyleDocument = css`
+  text-align: initial;
+
+  h1 {
+    text-align: center;
+    font-size: 28px;
+    font-weight: 600;
+    margin-bottom: 24px;
+  }
+
+  h2 {
+    margin: 32px 0 12px;
+    font-size: 20px;
+    font-weight: 600;
+  }
+
+  li {
+    margin-left: 24px;
+    list-style: disc;
+    margin: 8px 0 8px 24px;
+    line-height: 24px;
+  }
+`;
 
 const Document: React.FC<{ pathFile: string }> = ({ pathFile }) => {
   const [markdownText, setMarkdownText] = React.useState("");
@@ -18,7 +43,7 @@ const Document: React.FC<{ pathFile: string }> = ({ pathFile }) => {
   });
 
   return (
-    <div className='document'>
+    <div className='document' css={StyleDocument}>
       <ReactMarkdown source={markdownText} />
     </div>
   );
