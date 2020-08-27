e3876f758d8d6d6e6da29290021fc8917f2c7fe1
Naoki Mita <naoki.mita18@gmail.com>
2020-08-27 16:00:01 -0700

    Change default main light color

--- src/components/style/Color.ts
+++ src/components/style/Color.ts
@@ -2,7 +2,7 @@ export const Color = {
   Black: "#222",
   TextBlack: "#484848",
   MainColor: "#ffd31d",
-  MainLightColor: "#fae7cb",
+  MainLightColor: "#ffedb185",
   Gray: "#3e3e3eb8",
   LightGray: "#abababb8",
   DarkGray: "#3e3e3c",
